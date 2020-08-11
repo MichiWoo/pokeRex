aa707c06ded44b286dc746d2af2ccdb5ed355cad
MichiWoo <michiwoo.web@gmail.com>
2020-08-11 00:17:39 -0500

    Funciones asyncronas correctas

--- src/Containers/ListOfPokemons/index.js
+++ src/Containers/ListOfPokemons/index.js
@@ -3,39 +3,103 @@ import { Card } from '../../Components/Card'
 import { Container, Button } from './styles'
 
 export const  ListOfPokemons = () => {
-  const [result, setResult] = React.useState([])
-  const [poke, setPoke] = React.useState([])
-  const [load, setLoad] = React.useState('true')
+  const [result, setResult] = useState([])
+  const [poke, setPoke] = useState([])
+  const [load, setLoad] = useState('true')
+  const [limit, setLimit] = useState(20)
+  const [offset, setOffset] = useState(0)
   const arr = []
   
+
+
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon')
-      .then((response) => response.json())
-      .then((data) => setResult(
-        data.results.map((item) => {
-          fetch(item.url)
-            .then((response) => response.json())
-            .then((poke) => {
-              let myObj = {
-                id: parseInt(poke.id),
-                name: poke.name,
-                img: poke.sprites.other.dream_world.front_default,
-                type1: poke.types[0].type.name,
-                type2: poke.types[1] ? poke.types[0].type.name : ''
-              }
-              arr.push(myObj)
-            })
-          setPoke(arr)
-        }),
-      ))
-    }, [])
+    async function fecthPokeData() {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+      const data = await response.json()
+      setResult(data)
+      data.results.map((item) => {
+        fetch(item.url)
+          .then((response) => response.json())
+          .then((poke) => {
+            let myObj = {
+              id: parseInt(poke.id),
+              name: poke.name,
+              img: poke.sprites.other.dream_world.front_default,
+              type1: poke.types[0].type.name,
+              type2: poke.types[1] ? poke.types[0].type.name : ''
+            }
+            arr.push(myObj)
+          })
+        setPoke(arr)
+      })
+    }
+    fecthPokeData()
     setTimeout(() => {
       setLoad(false)
     }, 1500)
+  },[])
   
-  const handleNext = () => {
-    console.log('Here')
+  const handleNext = async () => {
+    console.log('Here Next ✋')
+    setLoad(true)
+    let _offset = offset + 20
+    setOffset(_offset)
+    const link = `https://pokeapi.co/api/v2/pokemon?offset=${_offset}&limit=${limit}`
+    console.log(link)
+    const response = await fetch(link)
+    const data = await response.json()
+    setResult(data)
+    data.results.map((item) => {
+      fetch(item.url)
+        .then((response) => response.json())
+        .then((poke) => {
+          let myObj = {
+            id: parseInt(poke.id),
+            name: poke.name,
+            img: poke.sprites.other.dream_world.front_default,
+            type1: poke.types[0].type.name,
+            type2: poke.types[1] ? poke.types[0].type.name : ''
+          }
+          arr.push(myObj)
+        })
+      setPoke(arr)
+      setTimeout(() => {
+        setLoad(false)
+      }, 1500)
+    })
   }
+
+  const handlePrevious = async () => {
+    console.log('Here Previous ✋')
+    setLoad(true)
+    let _offset = offset - 20
+    _offset < 0 ? 0 : _offset
+    setOffset(_offset)
+    const link = `https://pokeapi.co/api/v2/pokemon?offset=${_offset}&limit=${limit}`
+    console.log(link)
+    const response = await fetch(link)
+    const data = await response.json()
+    setResult(data)
+    data.results.map((item) => {
+      fetch(item.url)
+        .then((response) => response.json())
+        .then((poke) => {
+          let myObj = {
+            id: parseInt(poke.id),
+            name: poke.name,
+            img: poke.sprites.other.dream_world.front_default,
+            type1: poke.types[0].type.name,
+            type2: poke.types[1] ? poke.types[0].type.name : ''
+          }
+          arr.push(myObj)
+        })
+      setPoke(arr)
+      setTimeout(() => {
+        setLoad(false)
+      }, 1500)
+    })
+  }
+
   return (
     <Container>
       {
@@ -52,7 +116,10 @@ export const  ListOfPokemons = () => {
                     <Card key={i} pokemon={pokemon} />
                 ))
               }
-              <Button onClick={handleNext}>Next</Button>
+              {
+                offset > 0 ? <Button onClick={handlePrevious}>Atrás</Button> : null
+              }
+              <Button onClick={handleNext}>Siguiente</Button>
             </>
           )
       }
